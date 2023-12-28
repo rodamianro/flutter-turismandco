d465e78e0e6b80ea336e2ad559c9e5ca0b84bf2b
Robinson Rosales <developerobinsonrosales@gmail.com>
2023-12-27 22:15:29 -0500

    Make models serialized to Json - JSON serialization for location - JSON serialization for location_fact - Add location generated class - Add location_fact generated class

--- lib/models/location.dart
+++ lib/models/location.dart
@@ -1,9 +1,17 @@
 import "location_fact.dart";
+import "package:json_annotation/json_annotation.dart";
 
+part 'location.g.dart';
+
+@JsonSerializable()
 class Location {
   final String name;
   final String url;
   final List<LocationFact> facts;
 
   const Location({required this.name, required this.url, required this.facts});
+
+  factory Location.fromJson(Map<String, dynamic> json) =>
+      _$LocationFromJson(json);
+  Map<String, dynamic> toJson() => _$LocationToJson(this);
 }
--- lib/models/location_fact.dart
+++ lib/models/location_fact.dart
@@ -1,6 +1,14 @@
+import 'package:json_annotation/json_annotation.dart';
+part 'location_fact.g.dart';
+
+@JsonSerializable()
 class LocationFact {
   final String title;
   final String text;
 
   const LocationFact({required this.title, required this.text});
+
+  factory LocationFact.fromJson(Map<String, dynamic> json) =>
+      _$LocationFactFromJson(json);
+  Map<String, dynamic> toJson() => _$LocationFactToJson(this);
 }
